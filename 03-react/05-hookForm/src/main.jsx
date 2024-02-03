576c0ddacbe1970f38449b77580d9682b78f02b6
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-02-03 19:14:45 +0100

    Create structure to start working

---
+++ 03-react/05-hookForm/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FormView from './views/FormView'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('form')).render(
+  <React.StrictMode>
+    <FormView />
+  </React.StrictMode>,
+)
---
+++ 03-react/05-hookForm/src/views/FormView.jsx
@@ -0,0 +1,10 @@
+import Form from "../components/Form";
+
+function FormView() {
+
+    return (
+        <Form />
+    )
+}
+
+export default FormView;
