bb3ea5e9854fad6f2d150fea742a580e9d820bb9
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-02-07 11:02:41 +0100

    Start practicing katas

--- 03-react/03-landing_page_angy/README.md
+++ 03-react/03-landing_page_angy/README.md
@@ -1,8 +1,3 @@
-# React + Vite
+# Landing page + Katas React.js
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Created a landing page to practice, and then started using this repository to do React.js katas.
--- 03-react/03-landing_page_angy/src/components/app/App.jsx
+++ 03-react/03-landing_page_angy/src/components/app/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  let [ count, setCount ] = useState(0);
+
+  // tenemos que crear handleSetCount para que funcione, pues si no metemos setCount dentro de una función que se llame con handle, sale que setCount ya está inicializada
+  function handleSetCount() {
+    setCount(count+1);
+
+    let messageFiveTimes;
+
+    count === 5 ? messageFiveTimes = alert(`You've already clicked ${count} times!`) : null;
+  }
 
   return (
     <>
         <main>
             <h1>Este es el main</h1>
+            <button onClick={handleSetCount}>Click me</button>
+            <h2>You've clicked {count} times</h2>
         </main>
     </>
   )
